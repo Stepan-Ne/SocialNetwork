2235820a2d7e7573429f47b6807fe7ce55fc2ab0
step_ne <nemail.necom@gmail.com>
2020-05-13 23:13:58 +0300

    Add some items in the Dialogs.js and classes in the Dialogs.module.css

--- src/Content/Dialogs/Dialogs.js
+++ src/Content/Dialogs/Dialogs.js
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div className={s.item}>
-           Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Luba
+                </div>
+                <div className={s.dialog}>
+                    Olga
+                </div>
+                <div className={s.dialog}>
+                    Nicholas
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div>
+                    Hi there!
+                </div>
+                <div>
+                    How do you do
+                </div>
+                <div>
+                    I`m fine!
+                </div>
+            </div>
         </div>
     );
 }
