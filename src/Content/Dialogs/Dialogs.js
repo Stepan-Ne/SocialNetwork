2332802007451da48860a2f644ec70920f211bf4
step_ne <nemail.necom@gmail.com>
2020-05-14 10:11:35 +0300

    Add <NavLink to='/dialogs/(1-3)'>name</NavLink>

--- src/Content/Dialogs/Dialogs.js
+++ src/Content/Dialogs/Dialogs.js
@@ -1,18 +1,19 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog}>
-                    Luba
+                   <NavLink to='/dialogs/1'>Luba</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Olga
+                    <NavLink to='/dialogs/2'>Olga</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Nicholas
+                    <NavLink to='/dialogs/3'>Nicholas</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
