a5280c185c8db2059b3b15f5604f9b6cb5cf5b19
Stepan <nemail.necom@gmail.com>
2020-11-30 23:42:54 +0300

    validate for login & FormControl: textarea & input

--- src/Components/Common/FormsControls/FormsControls.js
+++ src/Components/Common/FormsControls/FormsControls.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import s from './renderedField.module.css';
 
-
-export const Textarea = ({input, meta, ...props}) => {
+export const FormControl = ({input, meta, ...props}) => {
     return (
         <div className={meta.touched && meta.error ? s.formControl : ''}>
-            <textarea {...input} placeholder={props.label}/>
+            {props.children}
             <div>
             {meta.touched && meta.error && 
             <span className={s.error}>{meta.error}</span>}
             </div>
-    
         </div>
     )
-};      
+};   
+
+export const Textarea = (props) => {
+    const {input, ...restProps} = props;
+    return (
+        <>
+        <FormControl {...props}>
+            <textarea {...input} placeholder={restProps.label} {...restProps}/>
+        </FormControl>
+        </>
+        
+    )
+};   
+
+export const Input = (props) => {
+    const {input, type, restProps} = props;
+    return <>
+    <FormControl {...props}>
+            <input {...input} type={type} {...restProps}/>
+    </FormControl>
+    </>
+}
--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form'
+import { Input } from '../Common/FormsControls/FormsControls';
+import { required } from '../utils/validator';
 
 const Login = (props) => {
 
@@ -19,13 +21,13 @@ const LoginForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div>
-      <Field name='login' component='input' type='text' placeholder='login'/>
+      <Field name='login' component={Input} validate={[required]} type='text' placeholder='login'/>
       </div>
       <div>
-        <Field name='password' component='input' type='text' placeholder='password'/>
+        <Field name='password' component={Input} validate={[required]} type='text' placeholder='password'/>
       </div>
       <div>
-        <Field name='rememberMy' component='input' type='checkbox' />remember me
+        <Field name='rememberMy' component={Input} type='checkbox' />remember me
       </div>
       <button>Log in</button>
     </form>
