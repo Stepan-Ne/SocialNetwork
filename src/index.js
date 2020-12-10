37cd767f7b546622fccd82f35a17e924e3ebf2bf
Stepan <nemail.necom@gmail.com>
2020-12-11 00:54:46 +0300

    HashRouter for gh-pages

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import store from "./Redux/redux-store";
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 
 
     ReactDOM.render(
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>,
+        </HashRouter>,
         document.getElementById('root')
     )
 
