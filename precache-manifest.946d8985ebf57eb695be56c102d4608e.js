45c2a732a1b97863478b13d8f7a91d40a50d0f19
Stepan <nemail.necom@gmail.com>
2021-01-29 03:05:22 +0300

    Updates

---
+++ precache-manifest.946d8985ebf57eb695be56c102d4608e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99764c9a80eb91b7b8a71f2f521c0a0b",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "f154bb57d8bdd7f169d8",
+    "url": "/SocialNetwork/static/css/3.376c6f01.chunk.css"
+  },
+  {
+    "revision": "fde35ad3cf53c06d840c",
+    "url": "/SocialNetwork/static/css/main.f79484c9.chunk.css"
+  },
+  {
+    "revision": "f4a1389532f49d4b2385",
+    "url": "/SocialNetwork/static/js/2.a87c3fa0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork/static/js/2.a87c3fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f154bb57d8bdd7f169d8",
+    "url": "/SocialNetwork/static/js/3.201aa2b3.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/SocialNetwork/static/js/3.201aa2b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde35ad3cf53c06d840c",
+    "url": "/SocialNetwork/static/js/main.1a5365fa.chunk.js"
+  },
+  {
+    "revision": "7ffae0094a6b8620de07",
+    "url": "/SocialNetwork/static/js/runtime-main.ad73ea8f.js"
+  }
+]);
